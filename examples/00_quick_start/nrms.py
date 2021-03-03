ec8639ca7052bbc36123bda36aa6f45d9efff01c
Yutian Zhao <u6489809@anu.edu.au>
2021-03-03 18:00:04 +0800

    exp long 3

--- examples/00_quick_start/nrms.py
+++ examples/00_quick_start/nrms.py
@@ -56,7 +56,7 @@ class PositionalEncoding(nn.Module):
         # Compute the positional encodings once in log space.
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2, dtype="torch.float") *
+        div_term = torch.exp(torch.arange(0, d_model, 2, dtype=torch.float) *
                              -(math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
